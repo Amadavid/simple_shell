c2a9fd694401ff34a939191cce631b5e314211b8
amadavid <anihgrace200@gmail.com>
2023-11-15 06:13:45 -0800

    amadavid

--- putcharr.c
+++ putcharr.c
@@ -2,15 +2,15 @@
 
 /**
 * putcharr - writes the character to a specified file descriptor
-* 
+*
 * @fd: The file descriptor to be given when writing bytes
-* 
+*
 * @character: The character to print
 * Return: On success 1.
 * On error, -1 is returned, and errno is set.
 */
 int putcharr(char character, int fd)
 {
-// This function is used to write characters to the console.
+/* This function is used to write characters to the console.*/
 return (write(fd, &character, 1));
 }
--- shell.h
+++ shell.h
@@ -3,7 +3,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
 
+extern char **environ;
 
 /* Function prototypes */
 void prompt(void);
